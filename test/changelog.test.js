bcffdb8af4c0d1713b28693b4930b872de00c0ac
Camilo Alejandro Moreno Piñeros <cmorenop@fundaciongruposocial.co>
2023-09-05 16:54:32 -0500

    chore(tests): for writeChangelog, generateContent

--- test/changelog.test.js
+++ test/changelog.test.js
@@ -1,9 +1,43 @@
 const fs = require('fs');
-const { writeChangelog, generateChangelog, createCurrentReleaseFile, writeSpecFile } = require('../lib/changelog');
+const { writeChangelog, generateChangelog, generateContent, writeSpecFile } = require('../lib/changelog');
 const date = require('../lib/utils');
 const defaultValues = require('../default');
 
 jest.mock('fs');
+
+describe('writeChangelog', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should write content to the changelog file', () => {
+        fs.existsSync.mockReturnValue(true);
+        let content = '## VERSIONING: version 1.0.0 (2023-09-04)'
+        expect(writeChangelog(content)).toBe(true);
+        expect(fs.existsSync).toHaveBeenCalledWith(defaultValues.releasesFile);
+        expect(fs.readFileSync).toHaveBeenCalledWith(defaultValues.releasesFile, 'utf-8');
+        expect(fs.writeFileSync).toHaveBeenCalledWith(
+            defaultValues.releasesFile,
+            expect.stringContaining('## VERSIONING: version 1.0.0 (2023-09-04)'),
+            'utf-8'
+        );
+    }); 
+});
+
+describe('getCurrentDate', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should return the current date in the "YYYY-MM-DD" format', () => {
+        const mockDate = new Date('2024-09-04T12:00:01Z');
+        const originalDate = global.Date;
+        global.Date = jest.fn(() => mockDate);
+        const currentDate = date.getCurrentDate();
+        global.Date = originalDate;
+        expect(currentDate).toBe('2024-09-04');
+    });
+});
 
 describe('writeFile', () => {
     beforeEach(() => {
@@ -11,11 +45,8 @@ describe('writeFile', () => {
     });
 
     it('should write content to a specific file', () => {
-        // fs.existsSync.mockReturnValue(true);
         let content = '## VERSIONING: version 1.0.0 (2023-09-04)'
         expect(writeSpecFile(defaultValues.releasesFile, content)).toBe(true);
-        // expect(fs.existsSync).toHaveBeenCalledWith(defaultValues.releasesFile);
-        // expect(fs.readFileSync).toHaveBeenCalledWith(defaultValues.releasesFile, 'utf-8');
         expect(fs.writeFileSync).toHaveBeenCalledWith(
             defaultValues.releasesFile,
             expect.stringContaining('## VERSIONING: version 1.0.0 (2023-09-04)'),
@@ -28,9 +59,7 @@ describe('writeFile', () => {
         fs.writeFileSync.mockImplementation(() => {
             throw new Error('Error');
         });
-
         const result = writeSpecFile(defaultValues.releasesFile, content);
-
         expect(fs.writeFileSync).toHaveBeenCalledWith(defaultValues.releasesFile, content, 'utf-8');
         // expect(console.error).toHaveBeenCalledWith('Error appending to file:', error);
         expect(result).toBe(false);
@@ -75,5 +104,27 @@ describe('generateChangelog', () => {
             "feature": [],
             "test": []
         });
+    });
+});
+
+describe('generateContent', () => {
+    const changelog = {
+        "feat": [
+            {
+                hash: 'bcc665f',
+                scope: 'message',
+                message: ' add git origin to message of commit',
+                cleanedRemoteUrl: 'https://github.com/ZOLUXERO/update-this-version'
+            }
+        ],
+    }
+
+    it('should return content based on the changelog', () => {
+        const mockGetCurrentDate = jest.fn().mockReturnValue('2024-09-04');
+        date.getCurrentDate = mockGetCurrentDate;
+        const content = generateContent(changelog, '0.0.1');
+        expect(content).toContain('## VERSIONING: version 0.0.1 (2024-09-04)');
+        expect(content).toContain('### feat:');
+        expect(content).toContain('- **message**  add git origin to message of commit ([bcc665f](https://github.com/ZOLUXERO/update-this-version/commit/bcc665f))');
     });
 });
